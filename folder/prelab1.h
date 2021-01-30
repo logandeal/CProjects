0771e16f5545e0985beebcd7de624a8e63e09efb
Logan Deal <53441579+logandeal@users.noreply.github.com>
2021-01-30 11:34:07 -0600

    Delete folder directory

--- folder/prelab1.h
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-
-#include <stdlib.h>
-
-int findBigSmall(int arraySize, float array[], float* userNum, float* largestInArrayLessThanUser, float* smallestInArrayGreaterThanUser);
